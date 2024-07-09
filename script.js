8a86b9213e457838df32203a4e16acfe9754182c
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-09 15:49:51 -0700

    Add exercise 5

--- script.js
+++ script.js
@@ -151,3 +151,67 @@ inventorNamedAda = inventors.find((inventor) => { return inventor.first === "Ada
 // Check your return value:
 console.log('Exercise 4 My Result: ', inventorNamedAda)
 console.log('Exercise 4 Correct Result: ', { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 })
+
+// Array.prototype.map()
+
+// 5. Use the Array.prototype.map() method to reformat each name in the 'people' array. 
+// The goal is to convert names from "Last, First" format to "First Last" format.
+
+// Hint: Use the String.prototype.split method to separate the first and last names. You can split the string using ', ' as the separator.
+// After splitting the names, rearrange them to the "First Last" format.
+
+let firstLast = []
+
+// Enter your solution code here:
+
+firstLast = people.map((person) => {
+    const splitName = person.split(", ");
+    return `${splitName[1]} ${splitName[0]}`;
+});
+
+// Check your return value:
+console.log('Exercise 5 My Result: ', firstLast)
+console.log('Exercise 5 Correct Result: ',
+    [
+        'Carl Becker',
+        'Samuel Beckett',
+        'Mick Beddoes',
+        'Henry Beecher',
+        'Ludwig Beethoven',
+        'Menachem Begin',
+        'Hilaire Belloc',
+        'Saul Bellow',
+        'Robert Benchley',
+        'Peter Benenson',
+        'David Ben-Gurion',
+        'Walter Benjamin',
+        'Tony Benn',
+        'Chester Bennington',
+        'Leana Benson',
+        'Silas Bent',
+        'Lloyd Bentsen',
+        'Ric Berger',
+        'Ingmar Bergman',
+        'Luciano Berio',
+        'Milton Berle',
+        'Irving Berlin',
+        'Eric Berne',
+        'Sandra Bernhard',
+        'Yogi Berra',
+        'Halle Berry',
+        'Wendell Berry',
+        'Erin Bethea',
+        'Aneurin Bevan',
+        'Ken Bevel',
+        'Joseph Biden',
+        'Ambrose Bierce',
+        'Steve Biko',
+        'Josh Billings',
+        'Frank Biondo',
+        'Augustine Birrell',
+        'Elk Black',
+        'Robert Blair',
+        'Tony Blair',
+        'William Blake'
+    ]
+)
